d87d28f837ec97ef2989b427b853fd32c7ca1e96
Bob.Liu <bob.liu@webpowerchina.com>
2017-10-12 14:25:03 +0800

    iterator学习

--- Javascript/ES6-book/iterator.js
+++ Javascript/ES6-book/iterator.js
@@ -1,3 +1,10 @@
+/*
+* @Author: Bob.Liu
+* @Date:   2017-10-12 09:35:06
+* @Last Modified by:   Bob.Liu
+* @Last Modified time: 2017-10-12 14:22:46
+*/
+
 var it = makeIterator(['a', 'b']);
 
 console.log(it.next());
@@ -6,161 +13,112 @@ console.log(it.next());
 
 function makeIterator(array) {
   var nextIndex = 0;
+
   return {
     next: function () {
-      return nextIndex < array.length ? {value:array[nextIndex++],done: false} : {value:undefined,done:true};
+      return nextIndex < array.length ? {value: array[nextIndex++], done: false} : {value: undefined, done: true};
     }
   }
 }
 
-var obj = {
-  [Symbol.iterator] : function () {
-    return {
-      next: function () {
-        return {
-          value: 1,
-          doen: true
-        }
+var it = idMaker();
+
+console.log(it.next().value);
+console.log(it.next().value);
+console.log(it.next().value);
+
+function idMaker() {
+  var index = 0;
+
+  return {
+    next: function () {
+      return {value: index++, done: false}
+    }
+  }
+}
+
+var arr = [1,2,3,4,5,6,7];
+var iterator = arr[Symbol.iterator]();
+
+console.log(iterator.next());
+console.log(iterator.next());
+console.log(iterator.next());
+console.log(iterator.next());
+
+class RangeIterator {
+  constructor(start, stop) {
+    this.value = start;
+    this.stop = stop;
+  }
+  [Symbol.iterator]() {
+    return this;
+  }
+  next() {
+    var value = this.value;
+
+    if (value < this.stop) {
+      this.value++;
+      return {
+        done: false, 
+        value: value
       }
     }
+    return {
+      done: true,
+      value: undefined
+    }
   }
 }
-
-// class RangeIterator {
-//   constructor(start, stop) {
-//     this.value = start;
-//     this.stop = stop;
-//   }
-//   [Symbol.iterator]() {
-//     return this;
-//   }
-//   next() {
-//     var value = this.value;
-
-//     if (value < this.stop) {
-//       this.value++;
-//       return {
-//         done: false,
-//         value: value
-//       }
-//     }
-//     return {
-//       down: true,
-//       value: undefined
-//     }
-//   }
-// }
-// function range(start, stop) {
-//   return new RangeIterator(start, stop);
-// }
-// for (var value of range(0, 3)) {
-//   console.log(value);
-// }
-
-
-// function Obj(value) {
-//   this.value = value;
-//   this.next = null;
-// }
-// Obj.prototype[Symbol.iterator] = function () {
-//   var iterator = {next: next};
-//   var current = this;
-
-//   function next() {
-//     if (current) {
-//       var value = current.value;
-
-//       current = current.next;
-//       return {
-//         done: false,
-//         value: value
-//       }
-//     } else {
-//       return {
-//         down: true
-//       }
-//     }
-//   }
-//   return iterator;
-// }
-
-// var one = new Obj(1);
-// var two = new Obj(2);
-// var three = new Obj(3);
-
-// for (var i of one) {
-//   console.log(i);
-// }
-
-var set = new Set().add('a').add('b').add('c');
-var [x, ...y] = set;
-console.log(x, y);
-
-var generator = function *() {
-  yield 1;
-  yield* [2,3,4];
-  yield 5;
+function range(start, stop) {
+  return new RangeIterator(start, stop);
 }
-var iterator = generator();
+for (var value of range(0, 3)) {
+  console.log(value);
+}
 
-console.log(iterator.next());
-console.log(iterator.next());
-console.log(iterator.next());
-console.log(iterator.next());
-console.log(iterator.next());
-console.log(iterator.next());
-console.log(iterator.next());
-console.log(iterator.next());
 
+
+console.log([...'1234567890']);
 
 var myIterable = {};
 myIterable[Symbol.iterator] = function *() {
   yield 1;
   yield 2;
   yield 3;
-};
+}
 console.log([...myIterable]);
 
+
 var obj = {
-  [Symbol.iterator]: function*() {
+  *[Symbol.iterator]() {
     yield 'hello';
     yield 'world';
   }
 }
-for (let x of obj) {
+for (var x of obj) {
   console.log(x);
 }
 
-
-var arr = ['a', 'b', 'c', 'd'];
-for (let a in arr) {
-  console.log(a);
-}
-for (let a of arr.entries()) {
-  console.log(a);
+var arrayLike = {length:2,0:'a',1:'b'};
+for (var temp of [Array.from(arrayLike)]) {
+  console.log(temp);
 }
 
+var es6 = {
+  edition: 6,
+  committee: "TC39",
+  standard: "ECMA-262"
+};
 
-var es6 = new Map();
-es6.set('edition', 6);
-es6.set('committee', 'TC39');
-es6.set('standard', 'ECMA-262');
-
-for (var [name, value] of es6) {
-  console.log(name, value);
+for (var key of Object.keys(es6)) {
+  console.log(key);
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function *entries(obj) {
+  for (var key of Object.keys(es6)) {
+    yield [key, obj[key]];
+  }
+}
+for (var [key, value] of entries(es6)) {
+  console.log(key, '->', value);
+}
